949abf77a0e6945ad5b5335f35588cd601f9908a
alexisharris <alha7603@colorado.edu>
2021-10-29 12:35:37 -0600

    midterm Project clean up and bug fixes for submit on each slide

--- midtermProject/index.js
+++ midtermProject/index.js
@@ -1,26 +1,150 @@
 
 // checkbox code
-// listen for click on submit button
+// listen for click on submit button for each day:
+//Monday
 document.querySelector(".submit").addEventListener("click", checkboxesClicked);
   // start function
   function checkboxesClicked () {
-    // count all of the checkboxes that are checked
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    var container = document.querySelector('.slidePage');
+    // count all of the checkboxes that are checked on the page
+    var checkboxes = container.querySelectorAll('input[type="checkbox"]:checked');
     // get length of result from variable above. If no checkboxes checked
     if(checkboxes.length==0) {
-      alert("Self care is important. If you can't make any time for yourself today, make sure to make some for yourself tomorrow!")
+      alert("Self care is important. If you can't make any time for yourself today, make sure to make some for yourself tomorrow!");
     }
     // if one checkbox is checked
     else if (checkboxes.length == 1) {
-      alert("Yay! You did one self care item. Let's see if we can do even more tomorrow.");}
+      alert("Yay! You did one self care item. Let's see if we can do even more tomorrow.");
+       }
     // if two checkboxes are checked
     else if (checkboxes.length ==2) {
-      alert("Wow, two items of self care! Think we can hit all three tomorrow?");}
-    // if thre checkboxes are checked
+      alert("Wow, two items of self care! Think we can hit all three tomorrow?");
+       }
+    // if three checkboxes are checked
     else {
       alert("Congratulations on completing all three acts of self care for today! You deserve it!");
     }
   }
+// repeat function for each day of the week, limiting container to each specific day's content
+// Tuesday
+  document.querySelector(".submit2").addEventListener("click", checkboxesClicked2);
+    function checkboxesClicked2 () {
+      // change container from page to tuesday
+      var container2 = document.querySelector('.tuesday');
+      var checkboxes2 = container2.querySelectorAll('input[type="checkbox"]:checked');
+      console.log(checkboxes2.length);
+      if(checkboxes2.length==0) {
+        alert("Self care is important. If you can't make any time for yourself today, make sure to make some for yourself tomorrow!");
+      }
+      else if (checkboxes2.length == 1) {
+        alert("Yay! You did one self care item. Let's see if we can do even more tomorrow.");
+       }
+      else if (checkboxes2.length ==2) {
+        alert("Wow, two items of self care! Think we can hit all three tomorrow?");
+       }
+      else {
+        alert("Congratulations on completing all three acts of self care for today! You deserve it!");
+      }
+    }
+// repeat process for Wednesday
+  document.querySelector(".submit3").addEventListener("click", checkboxesClicked3);
+      function checkboxesClicked3 () {
+        var container3 = document.querySelector('.wednesday');
+        var checkboxes3 = container3.querySelectorAll('input[type="checkbox"]:checked');
+        console.log(checkboxes3.length);
+        if(checkboxes3.length==0) {
+          alert("Self care is important. If you can't make any time for yourself today, make sure to make some for yourself tomorrow!");
+        }
+        else if (checkboxes3.length == 1) {
+          alert("Yay! You did one self care item. Let's see if we can do even more tomorrow.");
+         }
+        else if (checkboxes3.length ==2) {
+          alert("Wow, two items of self care! Think we can hit all three tomorrow?");
+        }
+        else {
+          alert("Congratulations on completing all three acts of self care for today! You deserve it!");
+        }
+      }
+
+// repeat process for Thursday
+      document.querySelector(".submit4").addEventListener("click", checkboxesClicked4);
+          function checkboxesClicked4 () {
+            var container4 = document.querySelector('.thursday');
+            var checkboxes4 = container4.querySelectorAll('input[type="checkbox"]:checked');
+            console.log(checkboxes4.length);
+            if(checkboxes4.length==0) {
+              alert("Self care is important. If you can't make any time for yourself today, make sure to make some for yourself tomorrow!");
+            }
+            else if (checkboxes4.length == 1) {
+              alert("Yay! You did one self care item. Let's see if we can do even more tomorrow.");
+            }
+            else if (checkboxes4.length ==2) {
+              alert("Wow, two items of self care! Think we can hit all three tomorrow?");
+            }
+            else {
+              alert("Congratulations on completing all three acts of self care for today! You deserve it!");
+
+            }
+          }
+// repeat process for Friday
+          document.querySelector(".submit5").addEventListener("click", checkboxesClicked5);
+              function checkboxesClicked5 () {
+                var container5 = document.querySelector('.friday');
+                var checkboxes5 = container5.querySelectorAll('input[type="checkbox"]:checked');
+                console.log(checkboxes5.length);
+                if(checkboxes5.length==0) {
+                  alert("Self care is important. If you can't make any time for yourself today, make sure to make some for yourself tomorrow!");
+                }
+                else if (checkboxes5.length == 1) {
+                  alert("Yay! You did one self care item. Let's see if we can do even more tomorrow.");
+                }
+                else if (checkboxes5.length ==2) {
+                  alert("Wow, two items of self care! Think we can hit all three tomorrow?");
+                }
+                else {
+                  alert("Congratulations on completing all three acts of self care for today! You deserve it!");
+                }
+              }
+
+// repeat process for Saturday
+        document.querySelector(".submit6").addEventListener("click", checkboxesClicked6);
+              function checkboxesClicked6 () {
+                var container6 = document.querySelector('.saturday');
+                var checkboxes6 = container6.querySelectorAll('input[type="checkbox"]:checked');
+                console.log(checkboxes6.length);
+                if(checkboxes6.length==0) {
+                  alert("Self care is important. If you can't make any time for yourself today, make sure to make some for yourself tomorrow!");
+                }
+                else if (checkboxes6.length == 1) {
+                  alert("Yay! You did one self care item. Let's see if we can do even more tomorrow.");
+                }
+                else if (checkboxes6.length ==2) {
+                  alert("Wow, two items of self care! Think we can hit all three tomorrow?");
+                }
+                else {
+                  alert("Congratulations on completing all three acts of self care for today! You deserve it!");
+                }
+              }
+
+// repeat process for Sunday
+        document.querySelector(".submit7").addEventListener("click", checkboxesClicked7);
+              function checkboxesClicked7 () {
+                var container7 = document.querySelector('.sunday');
+                var checkboxes7 = container7.querySelectorAll('input[type="checkbox"]:checked');
+                console.log(checkboxes7.length);
+                if(checkboxes7.length==0) {
+                  alert("Self care is important. If you can't make any time for yourself today, make sure to make some for yourself tomorrow!");
+                }
+                else if (checkboxes7.length == 1) {
+                  alert("Yay! You did one self care item. Let's see if we can do even more tomorrow.");
+                }
+                else if (checkboxes7.length ==2) {
+                  alert("Wow, two items of self care! Think we can hit all three tomorrow?");
+                }
+                else {
+                  alert("Congratulations on completing all three acts of self care for today! You deserve it!");
+                }
+              }
 
 // slides code
 const track = document.querySelector(".slideTrack");
